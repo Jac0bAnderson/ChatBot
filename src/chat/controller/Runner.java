0990cc55541f2d5ab4d792c04120f83a358722e3
Jac0bAnderson <jake912235@gmail.com>
2015-10-21 08:28:35 -0600

    chatbot
    the basic structure is up

---
+++ src/chat/controller/Runner.java
@@ -0,0 +1,15 @@
+package chat.controller;
+
+public class Runner {
+/**
+ *It starts the  program
+ * @author Jake
+ * @param args
+ */
+	public static void main(String [] args)
+	{
+		ChatController myChatController = new ChatController();
+		myChatController.start();
+	}
+	
+}
