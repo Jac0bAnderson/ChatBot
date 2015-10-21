911f577a64c3ed1d6bfe629deebfd2c566524830
Jac0bAnderson <jake912235@gmail.com>
2015-10-21 09:12:01 -0600

    created a chat view status

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,9 +1,20 @@
 package chat.controller;
 
+import chat.view.ChatView;
+
+
+/**
+ * Application controller for the chatbot project
+ * @author Jake
+ * @version 1.11 10/21/15 fixed error
+ */
 public class ChatController {
 
+	private ChatView myDisplay;
+	
 	public void start()
 	{
 		
 	}
+	
 }
