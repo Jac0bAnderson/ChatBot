26bc46be350a71ae8b93f5904543134d0c44e4c4
Jac0bAnderson <jake912235@gmail.com>
2015-12-05 13:57:18 -0700

    Added a color change button

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,40 +1,61 @@
+//package 
 package chat.view;
 
+//imports 
 import javax.swing.*;
 import chat.controller.ChatController;
 import java.awt.event.*;
+import java.awt.Color;
+import java.awt.color.*;
 
 public class ChatPanel extends JPanel
 {
+//declarations
 private ChatController baseController;
 private SpringLayout baseLayout;
 private JButton chatButton;
+private JButton colorChanger;
 private JTextArea chatTextArea;
 private JTextField TextField;
+private int red;
+private int green;
+private int blue;
 
 public ChatPanel(ChatController baseController)
 {
 	this.baseController = baseController;
 	baseLayout = new SpringLayout();
-	chatButton = new JButton("Submit");
 	
+	//gives the chatButton text and a tooltip
+	chatButton = new JButton("Submit");
 	chatButton.setToolTipText("submits text to Chat Bot");
 	
+	//sets the starting text for the chat bots text area, gives it a tooltip and makes it so the user cant edit the chat bots text
 	chatTextArea = new JTextArea("ChatBot: Hello");
-	
 	chatTextArea.setToolTipText("This is where the chat bot responds");
 	chatTextArea.setEnabled(false);
 	
+	//sets the size of the users textfield and also gives it a tooltip
 	TextField = new JTextField("",25);
-	
 	TextField.setToolTipText("Type here");
 	
+	//the text  and the tooltip of the colorChanger Button
+	colorChanger = new JButton("Click for Colors");
+	colorChanger.setToolTipText("Click to change colors");
+	
+	// starting RGB colors
+	red = 255;
+	green = 255;
+	blue = 255;
+	
+	//calls the panel, layout, and listeners methods
 	setupPanel();
 	setupLayout();
 	setupListeners();
 }
 private void setupPanel()
-{
+{//puts GUI components in the panel
+	this.add(colorChanger);
 	this.add(chatTextArea);
 	this.add(TextField);
 	this.add(chatButton);
@@ -43,16 +64,18 @@ private void setupPanel()
 }
 	
 private void setupLayout()
-{
+{// tells the GUI components where to be on the panel
 	baseLayout.putConstraint(SpringLayout.NORTH, chatTextArea, 39, SpringLayout.NORTH, this);
 	baseLayout.putConstraint(SpringLayout.WEST, chatTextArea, 120, SpringLayout.WEST, this);
 	baseLayout.putConstraint(SpringLayout.NORTH, chatButton, 1, SpringLayout.NORTH, TextField);
 	baseLayout.putConstraint(SpringLayout.WEST, TextField, 10, SpringLayout.WEST, this);
 	baseLayout.putConstraint(SpringLayout.SOUTH, TextField, -10, SpringLayout.SOUTH, this);
 	baseLayout.putConstraint(SpringLayout.EAST, chatButton, -10, SpringLayout.EAST, this);
+	baseLayout.putConstraint(SpringLayout.NORTH, colorChanger, -1, SpringLayout.NORTH, TextField);
+	baseLayout.putConstraint(SpringLayout.EAST, colorChanger, -6, SpringLayout.WEST, chatButton);
 }
 	private void setupListeners()
-	{
+	{//sends the users texts and lets the chat bot respond
 		chatButton.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent clickEvent)
@@ -65,7 +88,27 @@ private void setupLayout()
 			}
 		
 		});
+		// changes the color of the panel when the changecolors button it clicked
+		colorChanger.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent clickEvent)
+					{
+						colorChange();
+						setBackground(new Color(red, green, blue));
+						
+						
+					}
+				});
 	}
+	//method that determines the RGB code  
+	private void colorChange()
+	{
+		
+		red = (int) (Math.random()*255);
+		green  = (int) (Math.random()*255);
+		blue  = (int) (Math.random()*255);
+	}
+	//getters and setters
 	public ChatController getBaseController() {
 		return baseController;
 	}
