4d0f6a93b5498f4c5f2cc93faae519b5a7c8ea89
Jac0bAnderson <jake912235@gmail.com>
2015-12-06 09:18:36 -0700

    Added documentation

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,27 +1,30 @@
+//package
 package chat.view;
-
+//imports
 import javax.swing.JFrame;
 import chat.controller.ChatController;
 
 
 public class ChatFrame extends JFrame
 {
+	//declaration
 private ChatPanel basePanel;
-
 private ChatController baseController;
+
 public ChatFrame(ChatController baseController)
 {
 	this.baseController = baseController;
 	basePanel = new ChatPanel(baseController);
 	setupFrame();
 }
+//sets the size, visibility, if its Resizable,  and the frames title
 private void setupFrame()
 {
 	this.setContentPane(basePanel);
-	this.setSize(600,600);        
-	this.setVisible(true);		
-    this.setResizable(true);   
-    this.setTitle("Chat Bot");  
+	this.setSize(600,600);        // sets the size of the frame when it first appears on the screen
+	this.setVisible(true);		//makes the frame visible
+    this.setResizable(true);    // lets the user change its size
+    this.setTitle("Chat Bot");  // the frames title
 }
 public ChatController getBaseController()
 {
