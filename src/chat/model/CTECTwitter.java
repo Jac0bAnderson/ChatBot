ff7766ace010da53dd4e1b93e5a3a1c58052d65e
Jac0bAnderson <jake912235@gmail.com>
2016-03-07 09:14:55 -0700

    Updated CTECTwitter.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,19 +1,31 @@
 package chat.model;
 
 import java.util.ArrayList;
+
 import twitter4j.*;
 /**
  * 
  * @author jand6944
- *@version 0.3
+ *@version 0.4
  */
 public class CTECTwitter 
 {
 private ArrayList<Status> statusList;
 private ArrayList<String> wordList;
+private Twitter chatbotTwitter;
 public CTECTwitter()
 {
 	statusList = new ArrayList <Status>();
+	wordList = new  ArrayList <String>();
 	
+}
+public void sendTweet(String tweet)
+{
+	try {
+		chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+	} catch (TwitterException e) {
+		
+		e.printStackTrace();
+	}
 }
 }
