3d676eb78d2bd30c4ea39416908dd5f809dee046
Jac0bAnderson <jake912235@gmail.com>
2016-03-07 09:04:56 -0700

    Added some more things to CTECTwitter.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -5,10 +5,15 @@ import twitter4j.*;
 /**
  * 
  * @author jand6944
- *@version .2
+ *@version 0.3
  */
 public class CTECTwitter 
 {
 private ArrayList<Status> statusList;
 private ArrayList<String> wordList;
+public CTECTwitter()
+{
+	statusList = new ArrayList <Status>();
+	
+}
 }
