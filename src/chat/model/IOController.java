50aa154989a557c7a28972c06a703f1738832000
Jac0bAnderson <jake912235@gmail.com>
2016-03-07 20:48:12 -0700

    Added a write method.

--- src/chat/model/IOController.java
+++ src/chat/model/IOController.java
@@ -1,8 +1,20 @@
 package chat.model;
 
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class IOController 
 {
-
+  private String user;
+	private String content;
+	private File file;
+	private FileWriter fileWrite;
+	
+	
+	public void writeText()
+	{
+	  
+	}
 }
